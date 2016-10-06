f045caceb2aaa7552e7a5d94f4a51e1882dd637a
Chris Sauve <chris.sauve@shopify.com>
2016-10-06 10:12:48 +0200

    Fix error

--- packages/react-universal/src/server/start.js
+++ packages/react-universal/src/server/start.js
@@ -1,10 +1,11 @@
 // @flow
 
+import {join} from 'path';
 import type {ConfigType} from '@lemonmade/react-universal-config';
 
 export default function start(config: ConfigType, ...rest: any) {
   // $FlowIgnore: need the dynamic require here.
-  const createServer = require(path.join(config.buildDir, 'server', 'main.js')).default;
+  const createServer = require(join(config.buildDir, 'server', 'main.js')).default;
   const app = createServer(config, ...rest);
   const listener = app.listen(config.serverPort);
   return listener;
