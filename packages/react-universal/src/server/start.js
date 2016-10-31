17a7b917c2141c22358fa29c7aa0e4af07a3157f
Chris Sauve <chris.sauve@shopify.com>
2016-10-31 11:36:22 -0400

    Fix some flow issues

--- packages/react-universal/src/server/start.js
+++ packages/react-universal/src/server/start.js
@@ -3,10 +3,9 @@
 import {join} from 'path';
 import type {ConfigType} from '@lemonmade/react-universal-config';
 
-export default async function start(config: ConfigType, ...rest: any) {
+export default async function start(config: ConfigType, ...rest: any): Promise<net$Server> {
   // $FlowIgnore: need the dynamic require here.
   const createServer = require(join(config.buildDir, 'server', 'main.js')).default;
   const app = await createServer(config, ...rest);
-  const listener = app.listen(config.serverPort);
-  return listener;
+  return app.listen(config.serverPort);
 }
